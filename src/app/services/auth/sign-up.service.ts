9856ee6a664e77112fdef2dae42130a68dc5bff9
izmarius <izmarius.im@gmail.com>
2021-01-31 22:48:48 +0200

    added google sign up

--- src/app/services/auth/sign-up.service.ts
+++ src/app/services/auth/sign-up.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { UserService } from '../user/user.service';
 import { AuthStateChangeService } from './auth-state-change.service';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +16,35 @@ export class SignUpService {
     private authService: AuthStateChangeService
   ) { }
 
-  signUp(email, password): void { // todo: add check for user or doctor at signup
-    this.afAuth.createUserWithEmailAndPassword(email, password)
+  signUp(email, password): Promise<void> { // todo: add check for user or doctor at signup
+    return this.afAuth.createUserWithEmailAndPassword(email, password)
     .then((userCredentials) => {
+      this.sendVerificationEmail(userCredentials.user);
       this.userService.setUserData(userCredentials.user);
     })
     .catch((error) => {
       window.alert(error.message);
+    });
+  }
+
+  googleSignup(): Promise<void> {
+    return this.providerSignUp(new firebase.auth.GoogleAuthProvider());
+  }
+
+  private providerSignUp(provider): Promise<void> {
+    return this.afAuth.signInWithPopup(provider)
+    .then((userCredentials) => { // todo: redirect to dashboard
+      this.userService.setUserData(userCredentials.user);
+    })
+    .catch((error) => {
+      window.alert(error.message);
+    });
+  }
+
+  private sendVerificationEmail(user): Promise<void> {
+    return user.sendEmailVerification()
+    .then(() => {
+      window.alert('Validation email sent!'); // todo: change to romanian and add message into global messages
     });
   }
 }
