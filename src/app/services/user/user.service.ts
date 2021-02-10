689e719df8b452697ffd0ae8206b680bfd691661
izmarius <izmarius.im@gmail.com>
2021-02-11 00:09:48 +0200

    added crud users

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,14 +1,20 @@
+import { map, first } from 'rxjs/operators';
 import { UserDto } from './../../data/modelDTO/user-dto';
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { FirestoreService } from 'src/app/data/http/firestore.service';
+import { Observable } from 'rxjs';
+import { convertSnapshots } from 'src/app/data/utils/firestore-utils.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private USER_COLLECTION = 'user/';
 
   constructor(
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    private firestoreService: FirestoreService
   ) { }
 
   setUserData(user): Promise<void> {
@@ -17,6 +23,44 @@ export class UserService {
     userData.setUserEmail(user.email);
     return userRef.set(JSON.parse(JSON.stringify(userData)) , { // firestore does not accept custom objects
       merge: true
+    });
+  }
+
+  getAllUsers(): Observable<UserDto[]> {
+    return this.firestoreService.getCollection(this.USER_COLLECTION)
+    .pipe(
+      map((snaps) => convertSnapshots<UserService>(snaps)),
+      first()
+    );
+  }
+
+  createUser(userDto: UserDto): Promise<void> {
+    return this.firestoreService.saveDocumentByAutoId(this.USER_COLLECTION, userDto)
+    .then(() => {
+      window.alert('User created');
+    })
+    .catch((error) => {
+      window.alert(error.message);
+    });
+  }
+
+  updateUserInfo(userDto: UserDto, userId: string): Promise<void> {
+    return this.firestoreService.updateDocumentById(this.USER_COLLECTION, userId, userDto)
+    .then(() => {
+      window.alert('User updated with new info');
+    })
+    .catch((error) => {
+      window.alert(error.message);
+    });
+  }
+
+  deleteUser(userId: string): Promise<void> {
+    return this.firestoreService.deleteDocById(this.USER_COLLECTION, userId)
+    .then(() => {
+      window.alert('User deleted');
+    })
+    .catch((error) => {
+      window.alert(error.message);
     });
   }
 }
