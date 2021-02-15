3092aecd1cca86b7cbdf57ab1e04e9d1fa7a28a3
izmarius <izmarius.im@gmail.com>
2021-02-15 22:11:43 +0200

    tested crud user

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,3 +1,4 @@
+import { UserDto } from './../data/modelDTO/user-dto';
 import { LogInService } from './../services/auth/log-in.service';
 import { SignOutService } from './../services/auth/sign-out.service';
 import {Component, OnInit} from '@angular/core';
@@ -6,6 +7,7 @@ import {SignUpService} from '../services/auth/sign-up.service';
 import {DoctorAppointmentsService} from '../services/doc-appoinment-service/doctor-appointments.service';
 import {DoctorsAppointmentDTO} from '../data/modelDTO/doctors-appointment-dto';
 import {AnimalUtilInfo} from '../data/modelDTO/animal-util-info';
+import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-test',
@@ -19,7 +21,8 @@ export class TestComponent implements OnInit {
     public signUpService: SignUpService,
     private appointmentsService: DoctorAppointmentsService,
     public signOutService: SignOutService,
-    public logInService: LogInService
+    public logInService: LogInService,
+    private userService: UserService
   ) {
   }
 
@@ -31,6 +34,13 @@ export class TestComponent implements OnInit {
     // this.appointmentsService.createAppointment(this.getDoctorsAppointment(), 'o2Jt7YS9zCWvBfDWY08X');
     this.appointmentsService.deleteAppointment('8igbwSCLeaR6AW4ocbKR', 'o2Jt7YS9zCWvBfDWY08X');
     // this.appointmentsService.updateAppointment(this.getUpdatedDoctorsAppointment(), '7M7JRKmBlg8CxVveGOvW', 'o2Jt7YS9zCWvBfDWY08X');
+
+    // this.userService.createUser(this.getUserDto());
+    // this.userService.updateUserInfo(this.getUpdateUserDto(), 'b0Gkpo9nJJBrPL9gaHnD');
+    // this.userService.deleteUser('t6Ra4nekUVYhbM4TXDcbKyK4wUJ2');
+    // this.userService.getAllUsers().subscribe((res) => {
+    //   console.log(res);
+    // });
   }
 
   getUpdatedDoctorsAppointment(): DoctorsAppointmentDTO {
@@ -51,5 +61,23 @@ export class TestComponent implements OnInit {
       .setDateTime(new Date().toLocaleString())
       .setLocation('Str. Pasteour, bl5, nr. 22, app. 12')
       .setUserId('3NaCJrnyv7lMkXcmQUPM');
+  }
+
+  getUserDto(): UserDto {
+    const user = new UserDto();
+    user.setUserCity('Cluj');
+    user.setUserEmail('testuser1@mail.com');
+    user.setUserName('Marius');
+    user.setUserPhone('07412345678');
+    return user;
+  }
+
+  getUpdateUserDto(): UserDto {
+    const user = new UserDto();
+    user.setUserCity('Cluj');
+    user.setUserEmail('testuser2@mail.com');
+    user.setUserName('Ionut');
+    user.setUserPhone('0740123456');
+    return user;
   }
 }
