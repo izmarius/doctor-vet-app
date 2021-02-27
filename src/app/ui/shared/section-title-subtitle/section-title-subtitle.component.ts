352eaea93601b1c5d019fa6070a961c2fbaa0613
pausanionut <pausan@media-saturn.com>
2021-02-27 14:12:35 +0200

    Convert css to scss

--- src/app/ui/shared/section-title-subtitle/section-title-subtitle.component.ts
+++ src/app/ui/shared/section-title-subtitle/section-title-subtitle.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-section-title-subtitle',
   templateUrl: './section-title-subtitle.component.html',
-  styleUrls: ['./section-title-subtitle.component.css']
+  styleUrls: ['./section-title-subtitle.component.scss']
 })
 export class SectionTitleSubtitleComponent implements OnInit {
 
