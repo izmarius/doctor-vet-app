287fa99951849fa8030b51626c721e85a9445b0d
izmarius <izmarius.im@gmail.com>
2021-03-02 22:16:39 +0200

    changed css to scss

--- src/app/ui/shared/header-title/header-title.component.ts
+++ src/app/ui/shared/header-title/header-title.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header-title',
   templateUrl: './header-title.component.html',
-  styleUrls: ['./header-title.component.css']
+  styleUrls: ['./header-title.component.scss']
 })
 export class HeaderTitleComponent implements OnInit {
   @Input() subTitle: string;
