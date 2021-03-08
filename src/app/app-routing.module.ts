35ebb7aff43af89941403eb676e4e0c403958dfe
izmarius <izmarius.im@gmail.com>
2021-03-08 19:44:59 +0200

    spelling error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/doctor-appotiments', pathMatch: 'full'},
-  {path: 'doctor-appotiments', component: DoctorAppointmentsComponent}
+  {path: '', redirectTo: '/doctor-appointments', pathMatch: 'full'},
+  {path: 'doctor-appointments', component: DoctorAppointmentsComponent}
 ];
 
 @NgModule({
